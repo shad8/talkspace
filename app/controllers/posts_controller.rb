847aa7002dafc1290571d4344deddea87219b9a7
Urszula Hołodniak <uholodniak@gmail.org>
2016-04-13 00:35:10 +0200

    Add action to post controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+
+  def index
+    @posts = Post.all
+    respond_with @posts
+  end
+
+  def show
+    respond_with @post
+  end
+
+  def create
+    @post = Post.create(post_params)
+    respond_with @post
+  end
+
+  def update
+    @post.update(post_params)
+    respond_with @post
+  end
+
+  def destroy
+    @post.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
+  end
+end
---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :controller do
+  before { request.env['HTTP_ACCEPT'] = 'application/json' }
+  let(:user_post) { create(:post) }
+  let(:params) { attributes_for(:post) }
+
+  # TODO: All post by category
+  describe 'GET index' do
+    it 'returns http success' do
+      create(:post)
+      get :index
+      expect(response).to have_http_status(:success)
+      expect(response).to match_response_schema('posts')
+    end
+  end
+
+  describe 'GET show' do
+    it 'returns http success' do
+      get :show, params: { id: user_post }
+      expect(response).to have_http_status(:success)
+      expect(response).to match_response_schema('post')
+    end
+
+    it 'returns http not_found' do
+      rand_id = rand(5) + 1
+      get :show, params: { id: rand_id }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe 'POST create' do
+    it 'returns http created and save post' do
+      params[:user_id] = user_post.user.id
+      expect do
+        post(:create, params: { post: params })
+      end.to change(Post, :count).by(1)
+      expect(response).to have_http_status(:created)
+      expect(response).to match_response_schema('post')
+    end
+
+    it 'return http unprocessable_entity and not create post' do
+      params[:title] = ''
+      post :create, params: { post: params }
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
+  describe 'PUT update' do
+    it 'returns http no_content' do
+      params[:title] = rand_text 10
+      put :update, params: { id: user_post, post: params }
+      expect(response).to have_http_status(:no_content)
+    end
+
+    it 'returns http unprocessable_entity with empty title' do
+      params[:title] = ''
+      put :update, params: { id: user_post, post: params }
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
+  describe 'DELETE destroy' do
+    it 'returns http :no_content' do
+      post = create(:post)
+      expect do
+        delete :destroy, params: { id: post }
+      end.to change(Post, :count).by(-1)
+      expect(response).to have_http_status(:no_content)
+    end
+  end
+end
