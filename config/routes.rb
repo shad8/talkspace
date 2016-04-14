1862c2ed9a6878256476d1e06f66b1918c0e2698
Urszula Hołodniak <uholodniak@gmail.org>
2016-04-14 16:20:45 +0200

    r g controller categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+
+  get 'categories/show'
+
+  get 'categories/create'
+
+  get 'categories/update'
+
+  get 'categories/destroy'
+
   defaults format: :json do
     resources :users, except: [:new, :edit]
     resources :posts, except: [:new, :edit]
