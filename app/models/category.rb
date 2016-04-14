d563d8457e5b6da7438f360ed5148133ec6669d2
Urszula Hołodniak <uholodniak@gmail.org>
2016-04-14 21:27:26 +0200

    Add has_many and belongs_to

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-
-  get 'categories/show'
-
-  get 'categories/create'
-
-  get 'categories/update'
-
-  get 'categories/destroy'
-
   defaults format: :json do
-    resources :users, except: [:new, :edit]
+    resources :categories, except: [:new, :edit]
     resources :posts, except: [:new, :edit]
+    resources :users, except: [:new, :edit]
   end
 end
