e92dca802860ef3901308471793a37271b317c55
Urszula Hołodniak <uholodniak@gmail.org>
2016-04-14 16:10:58 +0200

    r g model category name user:references

---
+++ app/models/category.rb
@@ -0,0 +1,6 @@
+class Category < ApplicationRecord
+  belongs_to :user
+  has_many :posts
+
+  validates :name, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412213654) do
+ActiveRecord::Schema.define(version: 20160414140107) do
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.string   "body"
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_index "categories", ["user_id"], name: "index_categories_on_user_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+  end
+
+  add_index "posts", ["category_id"], name: "index_posts_on_category_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
