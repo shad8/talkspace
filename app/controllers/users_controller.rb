7f03f9748d986f425ce0e004d02173a2d5bde35a
Urszula Hołodniak <uholodniak@gmail.org>
2016-04-16 04:26:40 +0200

    Add role to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
+  before_action :check_permission, except: [:index, :show, :create]
 
   def index
     @users = User.all
@@ -27,11 +28,21 @@ class UsersController < ApplicationController
 
   private
 
+  def check_permission
+    return render_forbidden unless @user == session_user
+  end
+
   def set_user
     @user = User.find(params[:id])
   end
 
   def user_params
     params.require(:user).permit(:email, :login, :password)
+  end
+
+  # TODO: Refactoring
+  def session_user
+    session = Session.find_by(token: user_token)
+    session ? session.user : nil
   end
 end
