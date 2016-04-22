7161c294831939d963677a731e5b7181b578c0ac
Urszula Hołodniak <uholodniak@gmail.org>
2016-04-22 21:48:34 +0200

    Add more flexible api versionig

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   defaults format: :json do
-    scope module: 'api', constraints: { subdomain: 'api' } do
-      namespace :v1 do
+    scope module: :api, constraints: { subdomain: 'api' } do
+      scope module: :v1, constraints: ApiVersion.new(version: 1, default: true) do
         resources :categories, except: [:new, :edit]
         resources :posts, except: [:new, :edit]
         resources :users, except: [:new, :edit]
