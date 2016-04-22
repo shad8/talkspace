48e22f500e5506050491f58403b215ec04bf6dfb
Urszula Hołodniak <uholodniak@gmail.org>
2016-04-22 22:56:10 +0200

    Add Shoulda::Matchers.configure

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,3 +14,10 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.include FactoryGirl::Syntax::Methods
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
