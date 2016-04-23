028c60b9ed2495a3973e463d192ec557a811451e
Urszula Hołodniak <uholodniak@gmail.org>
2016-04-23 22:54:28 +0200

    rails g migration AddTokenToUsers token:string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415223527) do
+ActiveRecord::Schema.define(version: 20160423203216) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20160415223527) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.integer  "role",            default: 0
+    t.string   "token"
   end
 
 end
